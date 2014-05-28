7b21e285cb4835b8351681967eae3302de264532
Enrique Ramirez <enrique.ramirez35@gmail.com>
2014-05-28 10:04:42 -0600

    Added NFC Fragments and NFC Utilities

--- app/src/main/java/com/cita/wallet/app/NFCACTIVITY.java
+++ app/src/main/java/com/cita/wallet/app/NFCACTIVITY.java
@@ -1,29 +1,67 @@
 package com.cita.wallet.app;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+
+import com.cita.wallet.app.fragments.NativeNFCTransaction;
+import com.cita.wallet.app.fragments.NoNFCFragment;
+import com.cita.wallet.app.utils.NFCUtils;
 
 
-
-public class NFCACTIVITY extends Activity {
+public class NfcActivity extends Activity implements NativeNFCTransaction.OnNativeNFCFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfcactivity);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            getActionBar().setTitle(extras.getString("title"));
         }
+
+        if (NFCUtils.isNFCEnabled(this)) {
+            // nfc is available and enabled
+
+            Log.v("NFC", "has and is enabled");
+
+            //TODO: replace fragments
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, NativeNFCTransaction.newInstance("nfc", ""))
+                    .commit();
+
+        } else {
+
+            // TODO: Notify the user that he doesn't have nfc enabled
+
+            // Check if the user has Gemalto MicroSD Card
+            Log.v("NFC", "has a micro sd");
+
+            if (NFCUtils.hasMicroSD()) {
+
+                //TODO:Replace Fragment
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(
+                                R.id.container,
+                                NativeNFCTransaction.newInstance("sd", ""))
+                        .commit();
+            }
+
+        }
+
+        if (NFCUtils.hasNoNFC(this)) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container,
+                            new NoNFCFragment()).commit();
+        }
+
     }
 
 
@@ -46,19 +84,8 @@ public class NFCACTIVITY extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    @Override
+    public void onNFCFragmentInteraction(Uri uri) {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_nfcactivity, container, false);
-            return rootView;
-        }
     }
 }
